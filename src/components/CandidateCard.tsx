61f6aa8ff0b10b4e2859d5b069246babbcdcf5bd
Jacobus <jacobus.berghoefer@gmail.com>
2025-02-11 17:55:30 -0600

    fixed company displaying twice

--- src/components/CandidateCard.tsx
+++ src/components/CandidateCard.tsx
@@ -15,7 +15,7 @@ const CandidateCard: React.FC<{ candidate: Candidate }> = ({ candidate }) => {
         </h2>
         <p><strong>Location:</strong> {candidate.location || 'N/A'}</p>
         <p><strong>Email:</strong> {candidate.email || 'N/A'}</p>
-        <p><strong>Company:</strong> Company: {candidate.company || 'N/A'}</p>
+        <p><strong>Company:</strong> {candidate.company || 'N/A'}</p>
         <a href={candidate.html_url} target='_blank' rel='noopener noreferrer'>GitHub Profile</a>
       </div>
     </div>
